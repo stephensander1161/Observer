40884a20f38b26443ae207a6a412bd213c7b7d56
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-02-08 17:02:29 -0700

    added emailjs

--- api/server.js
+++ api/server.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import sendGrid from '@sendGrid/mail';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3030;
 
 app.use(bodyParser.json());
 
@@ -20,4 +23,30 @@ app.get('/api', (req, res, next) => {
 	res.send('API: running');
 });
 
-app.listen(3030, '0.0.0.0');
+app.post('/api/email', (req, res, next) => {
+	console.log(req.body);
+
+	sendGrid.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
+	const msg = {
+		to: 'stephen.sander1@gmail.com',
+		from: req.body.email,
+		subject: 'Website Contact',
+		text: req.body.message
+	};
+
+	sendGrid
+		.send(msg)
+		.then((result) => {
+			res.status(200).json({
+				success: true
+			});
+		})
+		.catch((err) => {
+			console.log('error: ', err);
+			res.status(401).json({
+				success: false
+			});
+		});
+});
+
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`));
--- observer/src/pages/contact/Contact.js
+++ observer/src/pages/contact/Contact.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState, } from 'react';
+import emailjs from 'emailjs-com';
 import './Contact.css';
 import { Button } from '../../components/Button';
 import  Footer  from '../../components/Footer'
 
+
 function Contact() {
+	const [messageData, setMessageData] = useState({ name: '', email: '', message: '', subject: '', disabled: false, emailSent: null});
+
+
+ function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_krhsxmg', 'template_izipfw9', e.target, 'user_AcjqpwbLJL2GOQ6YpqZ9g')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+	  e.target.reset();
+  } 
+
+
+
 	return (
     <>
 		<div className="page_bg">
 			<div className="contact-container">
 				<section className="contact-subscription">
-					<form>
-						<input type="text" name="name" placeholder="Your Name" className="contact-input" />
-						<input type="email" name="email" placeholder="Your Email" className="contact-input" />
+					<form onSubmit={sendEmail}>
+						<input value={messageData.name} type="text" name="name" placeholder="Your Name" className="contact-input" onChange={(e) => setMessageData({ ...messageData, name: e.target.value })} />
+						<input value={messageData.email} type="email" name="email" placeholder="Your Email" className="contact-input" onChange={(e) => setMessageData({ ...messageData, email: e.target.value })} />
+						<input value={messageData.subject} type="text" name="subject" placeholder="Subject" className="contact-input" onChange={(e) => setMessageData({ ...messageData, subject: e.target.value })} />
 
 						<textarea
+						value={messageData.message}
 							cols="30"
 							rows="5"
 							type="text"
-							name="email"
+							name="message"
 							placeholder="Your Message"
 							className="contact-input"
+							onChange={(e) => setMessageData({ ...messageData, message: e.target.value })}
 						/>
 
 						<Button buttonStyle="btn--outline">Send Email</Button>
+						{messageData.emailSent === true && <p>Email Sent</p>}
+						{messageData.emailSent === false && <p>Email Not Sent</p>}
+
 					</form>
 				</section>
 			
